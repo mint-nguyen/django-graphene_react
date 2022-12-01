57afa146be37a09274704a6c28d5186e02a20e11
Mint <mintnguyen.pa@gmail.com>
2022-11-30 17:25:17 -0800

    feat: update and delete tracks

--- backend/tracks/schema.py
+++ backend/tracks/schema.py
@@ -27,12 +27,54 @@ class CreateTrack(graphene.Mutation):
         user = info.context.user
 
         if user.is_anonymous:
-            raise Exception('Log in required')
+            raise Exception('Log in required!')
         track = Track(title=title, description=description,
                       url=url, posted_by=user)
         track.save()
         return CreateTrack(track=track)
 
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, track_id, title, description, url):
+        user = info.context.user
+
+        track = Track.objects.get(id=track_id)
+
+        if user != track.posted_by:
+            raise Exception('Not permitted to update track!')
+        track.title = title
+        track.description = description
+        track.url = url
+        track.save()
+        return UpdateTrack(track=track)
+
+
+class DeleteTrack(graphene.Mutation):
+    track_id = graphene.Int()
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+
+    def mutate(self, info, track_id):
+        user = info.context.user
+
+        track = Track.objects.get(id=track_id)
+
+        if user != track.posted_by:
+            raise Exception('Not permitted to delete track!')
+        track.delete()
+        return DeleteTrack(track_id=track_id)
+
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
+    delete_track = DeleteTrack.Field()
